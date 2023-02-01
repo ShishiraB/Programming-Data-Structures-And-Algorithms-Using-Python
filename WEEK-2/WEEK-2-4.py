1444e2ddd4fc440ebaa40f719ea9debf056dc16d
Shishira Bhat <shishira.21ai050@sode-edu.in>
2023-02-01 22:34:38 +0530

    Week-2-4

---
+++ WEEK-2/WEEK-2-4.py
@@ -0,0 +1,13 @@
+def mystery(l):
+  l[0:2] = l[3:5]
+  return()
+
+list1 = [34,17,12,88,53,97,62]
+mystery(list1)
+
+'''
+
+Here l[0:2]=l[3:5] mean that
+replace value of index 0 and 1 with values of index 3 and 4
+
+'''
