b0205d1737341034eb414a25c1a045cd741f8f67
Shishira Bhat <shishira.21ai050@sode-edu.in>
2023-02-01 23:08:44 +0530

    week-2-2 update

--- WEEK-2/WEEK-2-2.py
+++ WEEK-2/WEEK-2-2.py
@@ -4,4 +4,4 @@ u = x                       #u=[589,'big',397,'bash']
 w = y
 w = w[0:]
 w[0] = 357                  #w=[357,'bash']
-x[2:3] = [487]              #x=[589,'big',487,'bash']
+x[2:3] = [487]              #x=[589,'big',487,'bash'] therefore u=[589,'big',487,'bash']
