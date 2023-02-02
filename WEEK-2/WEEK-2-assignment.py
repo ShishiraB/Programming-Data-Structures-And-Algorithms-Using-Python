7e37643a915118d42b32206d1bed5ae418aaf0dd
Shishira Bhat <shishira.21ai050@sode-edu.in>
2023-02-02 23:35:43 +0530

    Assignment

---
+++ WEEK-2/WEEK-2-assignment.py
@@ -0,0 +1,40 @@
+#Question-1
+
+def primepartition(m):
+    if m <= 2:
+        return False
+    for i in range(2, m):
+        if is_prime(i) and is_prime(m-i):
+            return True
+    return False
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+#Question-2
+
+def matched(s):
+    stack = []
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            if stack[-1] == '(':
+                stack.pop()
+    return not stack
+
+
+#Question-3
+
+def rotatelist(l, k):
+    if k <= 0:
+        return l
+    k = k % len(l)
+    return l[k:] + l[:k]
