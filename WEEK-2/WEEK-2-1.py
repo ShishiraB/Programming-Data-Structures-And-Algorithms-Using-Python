ee0bf3764e253f81fc827cb8315e219d1e8e60d0
Shishira Bhat <shishira.21ai050@sode-edu.in>
2023-02-08 07:19:58 +0530

    Week-2-1 update

--- WEEK-2/WEEK-2-1.py
+++ WEEK-2/WEEK-2-1.py
@@ -9,4 +9,10 @@ x[0] = x[0][1:3]                # Statement 8
 w[4][0] = 1000                  # Statement 9
 a = (x[4][1] == 4)              # Statement 10
 
-# Here statement 8 gives error bcz we cannot add int as subscript over string 
+
+'''
+Here statement 8 gives error bcz we cannot add int as subscript over string 
+This error is caused because of statement 5 where it changes z[0] index to 0 
+since Z and X share same address the changes made in z will effect x so the value of x[0] will be set to 0
+when statement 8 will run that time because the value at 0th index is 0 so it throws error
+'''
